7caaa01dbe95970395f806afe60008aa71511e42
Nathan Sullivan <torrentails@users.noreply.github.com>
2015-05-21 19:21:17 +0930

    Update vidStart.bat
    Added feature to randomly chose a video, other improvements.

--- vidStart.bat
+++ vidStart.bat
@@ -1,4 +1,13 @@
 @echo off
-
+timeout /t 1
+setlocal EnableDelayedExpansion
+echo %RANDOM%+!random! >> nul
+set count=0
+for /f "delims=" %%a in ('dir /b *.mp4') do @(
+	set filename[!count!]=%%a
+	set /a count = count + 1
+)
+set /a choose = (%random% * 32768 + %random%) %% count
+set chosen=!filename[%choose%]!
 CD "C:\Program Files (x86)\VideoLAN\VLC"
-START /min /high vlc.exe --directx-hw-yuv --video-wallpaper --qt-start-minimized -I dummy --no-qt-notification --no-qt-name-in-title --no-video-title-show --no-subsdec-autodetect-utf8 --no-osd --play-and-exit "File:///<file_name_and_path_here>"
+START /min /BELOWNORMAL vlc.exe --directx-hw-yuv --video-wallpaper --qt-start-minimized -I dummy --no-qt-notification --no-qt-name-in-title --no-video-title-show --no-subsdec-autodetect-utf8 --no-osd --play-and-exit "File:///<FULL PATH OF THE VIDEOS>/%chosen%"
